fe917f651d7e70d564bc942ad486be64366b7fc8
lisadascse72 <iamlisadas2004@gmail.com>
2026-04-15 11:31:23 +0530

    Restructured into Week-1 folder

---
+++ Week-1/src/components/layout/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer style={{ background: "black", color: "white", padding: "10px", textAlign: "center" }}>
+      <p>© 2026 My App</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ Week-1/src/components/layout/Sidebar.jsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div style={{ width: "200px", background: "#f0f0f0", padding: "10px" }}>
+      <h3>Sidebar</h3>
+      <p>Menu Item 1</p>
+      <p>Menu Item 2</p>
+    </div>
+  );
+};
+
+export default Sidebar;
